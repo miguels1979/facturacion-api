7c6cd7fa0e9421e91cae9c2f1557dfb695df8bef
Miguel Angel Sosa <gravitones@hotmail.com>
2025-02-27 18:15:41 -0600

    Primer commit - Subiendo proyecto Spring Boot

---
+++ src/test/java/com/kosmostecnologia/facturador/FacturadorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kosmostecnologia.facturador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacturadorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
